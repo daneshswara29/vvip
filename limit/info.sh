50e17d27a4686f16bb7660fdbf5395e235d8e44a
daneshswara29 <44220709+daneshswara29@users.noreply.github.com>
2024-04-13 05:34:59 +0700

    Add files via upload

---
+++ limit/info.sh
@@ -0,0 +1,7 @@
+clear
+figlet -f 3d "Gretongers Vpn Premium Tunneling" | lolcat
+cat /root/log-install.txt
+echo "-----------------------------------------------------"
+echo "                Credit Script @amantubilah" | lolcat
+echo "                   Author Script anggun Vpn Premium" | lolcat
+echo "-----------------------------------------------------"
